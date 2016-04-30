5d85f1dc8c1460929dffa5e313f00ac23ef19fa9
sahinkemal <ksahin@ku.edu.tr>
2016-04-30 16:18:18 +0300

    Update bigNetworkWith1MData.jl

--- bigNetworkWith1MData.jl
+++ bigNetworkWith1MData.jl
@@ -14,14 +14,9 @@
 using Knet
 using ArgParse
 using JLD
-
-println("Knet is activated")
-println("JLD is activated ")
-
 @knet function wconv(x; out=0, window=0, paddingValue=0, cinit=Xavier())
   w = par(;init=cinit, dims=(window, window, 0, out))
   return conv(w,x; padding=paddingValue)
-
 end
 @knet function cbf(x; f=:relu,cwindow=0,cpadding=0,cout=0)
   y= wconv(x; window=cwindow, paddingValue=cpadding, out=cout)
@@ -57,7 +52,7 @@ function main()
     if epoch >= 1 && epoch < 9
       setp(model; lr=0.05)
     else
-      setp(model; adam=true, lr=0.01)
+      setp(model; lr=0.01)
     end
     println("epochNumber:%d\n",epoch)
     println("train is not successful")
